54329518fc1a60c6fc1da58c1b2f85d84f2711a7
apoorva <ssapoorva4@gmail.com>
2023-11-21 19:18:18 +0530

    icons,temp changes on future data

--- src/Weather/Body.js
+++ src/Weather/Body.js
@@ -6,12 +6,6 @@ import Cloud_icon from './Images/cloud-computing.png';
 import Drizzle_icon from './Images/drizzle.png';
 import Rain_icon from './Images/rain.png';
 import Snow_icon from './Images/snow.png';
-import Cloud1_icon from './Images/img1.png';
-import Cloud2_icon from './Images/img2.png';
-import Cloud3_icon from './Images/img3.png';
-import Cloud4_icon from './Images/img4.png';
-import Cloud5_icon from './Images/img5.png';
-import Cloud6_icon from './Images/img6.png';
 
 
 
@@ -20,6 +14,14 @@ function Body()
     const api_key = "9d2c282833cb71d207fb02e7c7e9ce8a";
 
     const [wicon,setWicon] = useState(Cloud_icon);
+    const[mon_icon, setmonicon] = useState(Cloud_icon);
+    const[tue_icon, settueicon] = useState(Cloud_icon);
+    const[wed_icon, setwedicon] = useState(Cloud_icon);
+    const[thu_icon, setthuicon] = useState(Cloud_icon);
+    const[fri_icon, setfriicon] = useState(Cloud_icon);
+    const[sat_icon, setsaticon] = useState(Cloud_icon);
+    const[sun_icon, setsunicon] = useState(Cloud_icon);
+
     const [weatherData, setWeatherData] = useState
     ({
         humidity: "",
@@ -30,25 +32,25 @@ function Body()
         temp_min: "",
         temp_max: "",
         icon: "01d",
-        Mon_icon: "",
+        Mon_icon: "01d",
         Mon_max: "",
         Mon_min: "",
-        Tue_icon: "",
+        Tue_icon: "01d",
         Tue_max: "",
         Tue_min: "",
-        Wed_icon: "",
+        Wed_icon: "01d",
         Wed_max: "",
         Wed_min: "",
-        Thu_icon: "",
+        Thu_icon: "01d",
         Thu_max: "",
         Thu_min: "",
-        Fri_icon: "",
+        Fri_icon: "01d",
         Fri_max: "",
         Fri_min: "",
-        Sat_icon: "",
+        Sat_icon: "01d",
         Sat_max: "",
         Sat_min: "",
-        Sun_icon: "",
+        Sun_icon: "01d",
         Sun_max: "",
         Sun_min: ""
     });
@@ -98,8 +100,8 @@ function Body()
                     windSpeed: Math.floor(data.list[i].wind.speed),
                     temperature:Math.floor((data.list[i].main.temp)-273.15) ,
                     description: data.list[i].weather[0].main,
-                    temp_min: Math.floor((data.list[i].main.temp_min)-273.15),
-                    temp_max: Math.floor((data.list[i].main.temp_max)-273.15),
+                    // temp_min: Math.floor((data.list[i].main.temp_min)-273.15),
+                    // temp_max: Math.floor((data.list[i].main.temp_max)-273.15),
                     icon: data.list[i].weather[0].icon,
                     Mon_icon: data.list[1].weather[0].icon,
                     Mon_max: Math.floor((data.list[1].main.temp_max)-273.15),
@@ -122,7 +124,6 @@ function Body()
                     sun_icon: data.list[7].weather[0].icon,
                     Sun_max: Math.floor((data.list[1].main.temp_max)-273.15),
                     Sun_min: Math.floor((data.list[0].main.temp_min)-273.15),
-
                 })
             }
             else
@@ -159,12 +160,229 @@ function Body()
         } 
         else if (weatherData.icon === "13d" || weatherData.icon === "13n") {
             setWicon(Snow_icon);
+           
         } 
         else {
             setWicon(Clear_icon);
         }
 
     },[weatherData])
+
+    useEffect(() => 
+    {
+        if (weatherData.Mon_icon === "01d" || weatherData.Mon_icon === "01n") {
+            setmonicon(Clear_icon);
+        } 
+        else if (weatherData.Mon_icon === "02d" || weatherData.Mon_icon === "02n") {          
+            setmonicon(Cloud_icon);           
+        } 
+        else if (weatherData.Mon_icon === "03d" || weatherData.Mon_icon === "03n") {
+            setmonicon(Drizzle_icon);
+        }
+        else if (weatherData.Mon_icon === "04d" || weatherData.Mon_icon === "04n") {
+            setmonicon(Drizzle_icon);
+        } 
+        else if (weatherData.Mon_icon === "09d" || weatherData.Mon_icon === "09n") {
+            setmonicon(Rain_icon);           
+        } 
+        else if (weatherData.Mon_icon === "10d" || weatherData.Mon_icon === "10n") {     
+            setmonicon(Rain_icon);    
+        } 
+        else if (weatherData.Mon_icon === "13d" || weatherData.Mon_icon === "13n") {  
+            setmonicon(Snow_icon);   
+        } 
+        else {
+            setmonicon(Clear_icon);
+        }
+
+    },[weatherData])
+
+
+    useEffect(() => 
+    {
+        if (weatherData.Tue_icon === "01d" || weatherData.Tue_icon === "01n") {
+            settueicon(Clear_icon);
+        } 
+        else if (weatherData.Tue_icon === "02d" || weatherData.Tue_icon === "02n") {          
+            settueicon(Cloud_icon);           
+        } 
+        else if (weatherData.Tue_icon === "03d" || weatherData.Tue_icon === "03n") {
+            settueicon(Drizzle_icon);
+        }
+        else if (weatherData.Tue_icon === "04d" || weatherData.Tue_icon === "04n") {
+            settueicon(Drizzle_icon);
+        } 
+        else if (weatherData.Tue_icon === "09d" || weatherData.Tue_icon === "09n") {
+            settueicon(Rain_icon);           
+        } 
+        else if (weatherData.Tue_icon === "10d" || weatherData.Tue_icon === "10n") {     
+            settueicon(Rain_icon);    
+        } 
+        else if (weatherData.Tue_icon === "13d" || weatherData.Tue_icon === "13n") {  
+            settueicon(Snow_icon);   
+        } 
+        else {
+            settueicon(Clear_icon);
+        }
+
+    },[weatherData])
+
+
+    useEffect(() => 
+    {
+        if (weatherData.Wed_icon === "01d" || weatherData.Wed_icon === "01n") {
+            setwedicon(Clear_icon);
+        } 
+        else if (weatherData.Wed_icon === "02d" || weatherData.Wed_icon === "02n") {          
+            setwedicon(Cloud_icon);           
+        } 
+        else if (weatherData.Wed_icon === "03d" || weatherData.Wed_icon === "03n") {
+            setwedicon(Drizzle_icon);
+        }
+        else if (weatherData.Wed_icon === "04d" || weatherData.Wed_icon === "04n") {
+            setwedicon(Drizzle_icon);
+        } 
+        else if (weatherData.Wed_icon === "09d" || weatherData.Wed_icon === "09n") {
+            setwedicon(Rain_icon);           
+        } 
+        else if (weatherData.Wed_icon === "10d" || weatherData.Wed_icon === "10n") {     
+            setwedicon(Rain_icon);    
+        } 
+        else if (weatherData.Wed_icon === "13d" || weatherData.Wed_icon === "13n") {  
+            setwedicon(Snow_icon);   
+        } 
+        else {
+            setwedicon(Clear_icon);
+        }
+
+    },[weatherData])
+
+
+    useEffect(() => 
+    {
+        if (weatherData.Thu_icon === "01d" || weatherData.Thu_icon === "01n") {
+            setthuicon(Clear_icon);
+        } 
+        else if (weatherData.Thu_icon === "02d" || weatherData.Thu_icon === "02n") {          
+            setthuicon(Cloud_icon);           
+        } 
+        else if (weatherData.Thu_icon === "03d" || weatherData.Thu_icon === "03n") {
+            setthuicon(Drizzle_icon);
+        }
+        else if (weatherData.Thu_icon === "04d" || weatherData.Thu_icon === "04n") {
+            setthuicon(Drizzle_icon);
+        } 
+        else if (weatherData.Thu_icon === "09d" || weatherData.Thu_icon === "09n") {
+            setthuicon(Rain_icon);           
+        } 
+        else if (weatherData.Thu_icon === "10d" || weatherData.Thu_icon === "10n") {     
+            setthuicon(Rain_icon);    
+        } 
+        else if (weatherData.Thu_icon === "13d" || weatherData.Thu_icon === "13n") {  
+            setthuicon(Snow_icon);   
+        } 
+        else {
+            setthuicon(Clear_icon);
+        }
+
+    },[weatherData])
+
+
+    useEffect(() => 
+    {
+        if (weatherData.Fri_icon === "01d" || weatherData.Fri_icon === "01n") {
+            setfriicon(Clear_icon);
+        } 
+        else if (weatherData.Fri_icon === "02d" || weatherData.Fri_icon === "02n") {          
+            setfriicon(Cloud_icon);           
+        } 
+        else if (weatherData.Fri_icon === "03d" || weatherData.Fri_icon === "03n") {
+            setfriicon(Drizzle_icon);
+        }
+        else if (weatherData.Fri_icon === "04d" || weatherData.Fri_icon === "04n") {
+            setfriicon(Drizzle_icon);
+        } 
+        else if (weatherData.Fri_icon === "09d" || weatherData.Fri_icon === "09n") {
+            setfriicon(Rain_icon);           
+        } 
+        else if (weatherData.Fri_icon === "10d" || weatherData.Fri_icon === "10n") {     
+            setfriicon(Rain_icon);    
+        } 
+        else if (weatherData.Fri_icon === "13d" || weatherData.Fri_icon === "13n") {  
+            setfriicon(Snow_icon);   
+        } 
+        else {
+            setfriicon(Clear_icon);
+        }
+
+    },[weatherData])
+
+
+    useEffect(() => 
+    {
+        if (weatherData.Sat_icon === "01d" || weatherData.Sat_icon === "01n") {
+            setsaticon(Clear_icon);
+        } 
+        else if (weatherData.Sat_icon === "02d" || weatherData.Sat_icon === "02n") {          
+            setsaticon(Cloud_icon);           
+        } 
+        else if (weatherData.Sat_icon === "03d" || weatherData.Sat_icon === "03n") {
+            setsaticon(Drizzle_icon);
+        }
+        else if (weatherData.Sat_icon === "04d" || weatherData.Sat_icon === "04n") {
+            setsaticon(Drizzle_icon);
+        } 
+        else if (weatherData.Sat_icon === "09d" || weatherData.Sat_icon === "09n") {
+            setsaticon(Rain_icon);           
+        } 
+        else if (weatherData.Sat_icon === "10d" || weatherData.Sat_icon === "10n") {     
+            setsaticon(Rain_icon);    
+        } 
+        else if (weatherData.Sat_icon === "13d" || weatherData.Sat_icon === "13n") {  
+            setsaticon(Snow_icon);   
+        } 
+        else {
+            setsaticon(Clear_icon);
+        }
+
+    },[weatherData])
+
+
+    useEffect(() => 
+    {
+        if (weatherData.Sun_icon === "01d" || weatherData.Sun_icon === "01n") {
+            setsunicon(Clear_icon);
+        } 
+        else if (weatherData.Sun_icon === "02d" || weatherData.Sun_icon === "02n") {          
+            setsunicon(Cloud_icon);           
+        } 
+        else if (weatherData.Sun_icon === "03d" || weatherData.Sun_icon === "03n") {
+            setsunicon(Drizzle_icon);
+        }
+        else if (weatherData.Sun_icon === "04d" || weatherData.Sun_icon === "04n") {
+            setsunicon(Drizzle_icon);
+        } 
+        else if (weatherData.Sun_icon === "09d" || weatherData.Sun_icon === "09n") {
+            setsunicon(Rain_icon);           
+        } 
+        else if (weatherData.Sun_icon === "10d" || weatherData.Sun_icon === "10n") {     
+            setsunicon(Rain_icon);    
+        } 
+        else if (weatherData.Sun_icon === "13d" || weatherData.Sun_icon === "13n") {  
+            setsunicon(Snow_icon);   
+        } 
+        else {
+            setsunicon(Clear_icon);
+        }
+
+    },[weatherData])
+
+
+
+
+
+
+
 
     useEffect(() => 
     {
@@ -213,7 +431,7 @@ function Body()
                     }}>
                         <div className='cloud'>
                             <h4>Mon</h4>
-                            <img src={Cloud1_icon} alt="cloud1-icon"></img>
+                            <img src={mon_icon} height={50} alt="cloud1-icon"></img>
                             <div className="forecastMax-Min_temp">
                             <p> {weatherData.Mon_max}</p>
                             <p> {weatherData.Mon_min}</p>
@@ -228,7 +446,7 @@ function Body()
                     }}>
                         <div className='cloud'>
                             <h4>Tue</h4>
-                            <img src={Cloud2_icon}></img>
+                            <img src={tue_icon} height={50}></img>
                             <div className="forecastMax-Min_temp">
                             <p> {weatherData.Tue_max}</p>
                             <p> {weatherData.Tue_min}</p>
@@ -241,7 +459,7 @@ function Body()
                     }}>
                         <div className='cloud'>
                             <h4>Wed</h4>
-                            <img src={Cloud3_icon}></img>
+                            <img src={wed_icon} height={50}></img>
                             <div className="forecastMax-Min_temp">
                             <p> {weatherData.Wed_max}</p>
                             <p> {weatherData.Wed_min}</p>
@@ -254,7 +472,7 @@ function Body()
                     }}>
                         <div className='cloud'>
                             <h4>Thu</h4>
-                            <img src={Cloud4_icon}></img>
+                            <img src={thu_icon} height={50}></img>
                             <div className="forecastMax-Min_temp">
                             <p> {weatherData.Thu_max}</p>
                             <p> {weatherData.Thu_min}</p>
@@ -267,7 +485,7 @@ function Body()
                     }}>
                         <div className='cloud'>
                             <h4>Fri</h4>
-                            <img src={Cloud5_icon}></img>
+                            <img src={fri_icon} height={50}></img>
                             <div className="forecastMax-Min_temp">
                             <p> {weatherData.Fri_max}</p>
                             <p> {weatherData.Fri_min}</p>
@@ -280,7 +498,7 @@ function Body()
                     }}>
                         <div className='cloud'>
                             <h4>Sat</h4>
-                            <img src={Cloud6_icon}></img>
+                            <img src={sat_icon} height={50}></img>
                             <div className="forecastMax-Min_temp">
                             <p> {weatherData.Sat_max}</p>
                             <p> {weatherData.Sat_min}</p>
@@ -288,20 +506,18 @@ function Body()
                         </div>
                     </button>
 
-
                     <button className="forecast-button"  onClick={() => {
                         return( seti(8), search() );
                     }}>
                         <div className='cloud'>
                             <h4>Sun</h4>
-                            <img src={Cloud6_icon}></img>
+                            <img src={sun_icon} height={50}></img>
                             <div className="forecastMax-Min_temp">
                             <p> {weatherData.Sun_max} </p>
                             <p> {weatherData.Sun_min} </p>
                             </div>
                         </div>
-                    </button>
-                    
+                    </button>  
                 </div>
             </div>
         </div>
