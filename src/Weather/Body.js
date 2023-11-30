5451ca15a614f1473b4eaa64b0954dd547b17251
apoorva <ssapoorva4@gmail.com>
2023-11-30 21:26:18 +0530

    first commit

--- src/Weather/Body.js
+++ src/Weather/Body.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import './Body.css';
 import search_icon from './Images/search.png';
+import Location_icon from './Images/location.png';
 import Clear_icon from './Images/clear.png';
 import Cloud_icon from './Images/cloud-computing.png';
 import Drizzle_icon from './Images/drizzle.png';
 import Rain_icon from './Images/rain.png';
 import Snow_icon from './Images/snow.png';
-
-
 
 function Body() 
 {
@@ -19,8 +18,6 @@ function Body()
     const[wed_icon, setwedicon] = useState(Cloud_icon);
     const[thu_icon, setthuicon] = useState(Cloud_icon);
     const[fri_icon, setfriicon] = useState(Cloud_icon);
-    const[sat_icon, setsaticon] = useState(Cloud_icon);
-    const[sun_icon, setsunicon] = useState(Cloud_icon);
 
     const [weatherData, setWeatherData] = useState
     ({
@@ -32,27 +29,27 @@ function Body()
         temp_min: "",
         temp_max: "",
         icon: "01d",
+        date: {},
+
         Mon_icon: "01d",
-        Mon_max: "",
-        Mon_min: "",
+        Mon_temp: "",
+        Mon_dt:{},
+        
         Tue_icon: "01d",
-        Tue_max: "",
-        Tue_min: "",
+        Tue_temp: "",
+        Tue_dt:{},
+        
         Wed_icon: "01d",
-        Wed_max: "",
-        Wed_min: "",
+        Wed_temp: "",
+        Wed_dt:{},
+        
         Thu_icon: "01d",
-        Thu_max: "",
-        Thu_min: "",
+        Thu_temp: "",
+        Thu_dt:{},
+        
         Fri_icon: "01d",
-        Fri_max: "",
-        Fri_min: "",
-        Sat_icon: "01d",
-        Sat_max: "",
-        Sat_min: "",
-        Sun_icon: "01d",
-        Sun_max: "",
-        Sun_min: ""
+        Fri_temp: "",
+        Fri_dt:{},
     });
     
     const [currentDate, setCurrentDate] = useState
@@ -65,6 +62,7 @@ function Body()
     let [i, seti] = useState(1)
 
     console.log(i)
+
     const search = async () => 
     {
         const element = document.getElementsByClassName("searchbar");
@@ -87,47 +85,42 @@ function Body()
 
             let data = await response.json();
 
-
             console.log(data)
 
             if (data) 
             {
-                setWeatherData
+            setWeatherData
                 ({
-
                     location: data.city.name,
                     humidity: data.list[i].main.humidity,
                     windSpeed: Math.floor(data.list[i].wind.speed),
                     temperature:Math.floor((data.list[i].main.temp)-273.15) ,
                     description: data.list[i].weather[0].main,
-                    // temp_min: Math.floor((data.list[i].main.temp_min)-273.15),
-                    // temp_max: Math.floor((data.list[i].main.temp_max)-273.15),
-                    icon: data.list[i-1].weather[0].icon,
+                    icon: data.list[i].weather[0].icon,
+                    date : DTCheck(data.list[i].dt_txt),
+
                     Mon_icon: data.list[5].weather[0].icon,
-                    Mon_max: Math.floor((data.list[5].main.temp_max)-273.15),
-                    Mon_min: Math.floor((data.list[5].main.temp_min)-273.15),
-                    Tue_icon: data.list[10].weather[0].icon,
-                    Tue_max: Math.floor((data.list[10].main.temp_max)-273.15),
-                    Tue_min: Math.floor((data.list[10].main.temp_min)-273.15),
-                    Wed_icon: data.list[15].weather[0].icon,
-                    Wed_max: Math.floor((data.list[15].main.temp_max)-273.15),
-                    Wed_min: Math.floor((data.list[15].main.temp_min)-273.15),
-                    Thu_icon: data.list[20].weather[0].icon,
-                    Thu_max: Math.floor((data.list[20].main.temp_max)-273.15),
-                    Thu_min: Math.floor((data.list[20].main.temp_min)-273.15),
-                    Fri_icon: data.list[25].weather[0].icon,
-                    Fri_max: Math.floor((data.list[25].main.temp_max)-273.15),
-                    Fri_min: Math.floor((data.list[25].main.temp_min)-273.15),
-                    Sat_icon: data.list[30].weather[0].icon,
-                    Sat_max: Math.floor((data.list[30].main.temp_max)-273.15),
-                    Sat_min: Math.floor((data.list[30].main.temp_min)-273.15),
-                    Sun_icon: data.list[35].weather[0].icon,
-                    Sun_max: Math.floor((data.list[35].main.temp_max)-273.15),
-                    Sun_min: Math.floor((data.list[35].main.temp_min)-273.15),
+                    Mon_temp: Math.floor((data.list[5].main.temp)-273.15),  
+                    Mon_dt: DTCheck(data.list[5].dt_txt),
+                    
+                    Tue_icon: data.list[13].weather[0].icon,
+                    Tue_temp: Math.floor((data.list[13].main.temp)-273.15),
+                    Tue_dt: DTCheck(data.list[13].dt_txt),
+                    
+                    Wed_icon: data.list[21].weather[0].icon,
+                    Wed_temp: Math.floor((data.list[21].main.temp)-273.15),
+                    Wed_dt: DTCheck(data.list[21].dt_txt),
+                    
+                    Thu_icon: data.list[29].weather[0].icon,
+                    Thu_temp: Math.floor((data.list[29].main.temp)-273.15),
+                    Thu_dt: DTCheck(data.list[29].dt_txt),
+                    
+                    Fri_icon: data.list[37].weather[0].icon,
+                    Fri_temp: Math.floor((data.list[37].main.temp)-273.15),
+                    Fri_dt: DTCheck(data.list[37].dt_txt),   
                 })
             }
             else
-
             {
                 console.error("Unexpected data format:", data);
             }
@@ -320,67 +313,9 @@ function Body()
 
     useEffect(() => 
     {
-        if (weatherData.Sat_icon === "01d" || weatherData.Sat_icon === "01n") {
-            setsaticon(Clear_icon);
-        } 
-        else if (weatherData.Sat_icon === "02d" || weatherData.Sat_icon === "02n") {          
-            setsaticon(Cloud_icon);           
-        } 
-        else if (weatherData.Sat_icon === "03d" || weatherData.Sat_icon === "03n") {
-            setsaticon(Drizzle_icon);
-        }
-        else if (weatherData.Sat_icon === "04d" || weatherData.Sat_icon === "04n") {
-            setsaticon(Drizzle_icon);
-        } 
-        else if (weatherData.Sat_icon === "09d" || weatherData.Sat_icon === "09n") {
-            setsaticon(Rain_icon);           
-        } 
-        else if (weatherData.Sat_icon === "10d" || weatherData.Sat_icon === "10n") {     
-            setsaticon(Rain_icon);    
-        } 
-        else if (weatherData.Sat_icon === "13d" || weatherData.Sat_icon === "13n") {  
-            setsaticon(Snow_icon);   
-        } 
-        else {
-            setsaticon(Clear_icon);
-        }
-
-    },[weatherData])
-
-
-    useEffect(() => 
-    {
-        if (weatherData.Sun_icon === "01d" || weatherData.Sun_icon === "01n") {
-            setsunicon(Clear_icon);
-        } 
-        else if (weatherData.Sun_icon === "02d" || weatherData.Sun_icon === "02n") {          
-            setsunicon(Cloud_icon);           
-        } 
-        else if (weatherData.Sun_icon === "03d" || weatherData.Sun_icon === "03n") {
-            setsunicon(Drizzle_icon);
-        }
-        else if (weatherData.Sun_icon === "04d" || weatherData.Sun_icon === "04n") {
-            setsunicon(Drizzle_icon);
-        } 
-        else if (weatherData.Sun_icon === "09d" || weatherData.Sun_icon === "09n") {
-            setsunicon(Rain_icon);           
-        } 
-        else if (weatherData.Sun_icon === "10d" || weatherData.Sun_icon === "10n") {     
-            setsunicon(Rain_icon);    
-        } 
-        else if (weatherData.Sun_icon === "13d" || weatherData.Sun_icon === "13n") {  
-            setsunicon(Snow_icon);   
-        } 
-        else {
-            setsunicon(Clear_icon);
-        }
-
-    },[weatherData])
-
-
-    useEffect(() => 
-    {
-        const currentDate = new Date();
+        const currentDate = new Date(
+            "2023-11-23 09:00:00"
+        );
         const options = { weekday: 'long', month: 'long', day: 'numeric', };
 
         setCurrentDate
@@ -392,23 +327,45 @@ function Body()
         });
     }, []);
 
+    function DTCheck(dt){
+        const currentDate = new Date(
+           dt
+        );  
+        let dT =
+        {
+            day: currentDate.getDate(),
+            month: currentDate.toLocaleDateString('en-US', { month: 'long' }),
+            dayOfWeek: currentDate.toLocaleDateString('en-US', { weekday: 'long' }),
+
+        };
+
+        return dT;
+    }
+
     return (
-        <div className='main-container'>
-            
+        <div className='main-container'> 
             <div className='frontpage'>
-                <div className="search">
+
+               <div className="search">
                     <input className='searchbar' type='text' placeholder='Search'></input>
                     <img src={search_icon} className="search-icon" alt="search-icon" onClick={search}></img>
                 </div>
+
                 <div className='main'>
                     <img src={wicon} className='current-weather-icon' alt="current-weather-icon"></img>
                     <div className='style1'>
                         <div className="weather-location">
+                            <img src={Location_icon} className="location-icon"></img>
                             <h1>{weatherData.location}</h1>
                         </div>
-                        <p className= "weather-forecast-date">{currentDate.dayOfWeek}, {currentDate.month} {currentDate.day}</p>
-                        <p>{weatherData.description}</p>
+
+                        <div className= "weather-forecast-date">
+                            <p> {weatherData.date.dayOfWeek} {weatherData.date.day} {weatherData.date.month}</p>
+                        </div>
+                       
+                        <p className= "weather-forecast-date">{weatherData.description}</p>
                     </div>
+
                     <div className="current-weather-info-wrapper">
                         <div className="units">{weatherData.temperature}°C</div>
                         <ul>
@@ -417,101 +374,68 @@ function Body()
                         </ul>
                     </div>
                 </div>
-                <hr></hr>
+                <hr className="hrline"></hr>
                 <div className='day-weather'>
                     
                     <button className="forecast-button"  onClick={() => {
                         return( seti(5), search() );
                     }}>
                         <div className='cloud'>
-                            <h4>Mon</h4>
-                            <img src={mon_icon} height={50} alt="cloud1-icon"></img>
-                            <div className="forecastMax-Min_temp">
-                            <p> {weatherData.Mon_max}</p>
-                            <p> {weatherData.Mon_min}</p>
-                           
-                            </div>
-                
-                        </div>
-                    </button>
-
-                    <button className="forecast-button"  onClick={() => {
-                        return( seti(10), search() );
-                    }}>
-                        <div className='cloud'>
-                            <h4>Tue</h4>
-                            <img src={tue_icon} height={50}></img>
-                            <div className="forecastMax-Min_temp">
-                            <p> {weatherData.Tue_max}</p>
-                            <p> {weatherData.Tue_min}</p>
+                            <h4>{weatherData.Mon_dt.day}</h4>
+                            <img src={mon_icon} height={60} alt="cloud1-icon"></img>
+                            <div className="forecast-temp">
+                                <p>{weatherData.Mon_temp}°C</p>
                             </div>
                         </div>
                     </button>
 
                     <button className="forecast-button"  onClick={() => {
-                        return( seti(15), search() );
+                        return( seti(13), search() );
                     }}>
                         <div className='cloud'>
-                            <h4>Wed</h4>
-                            <img src={wed_icon} height={50}></img>
-                            <div className="forecastMax-Min_temp">
-                            <p> {weatherData.Wed_max}</p>
-                            <p> {weatherData.Wed_min}</p>
+                            <h4>{weatherData.Tue_dt.day}</h4>
+                            <img src={tue_icon} height={60}></img>
+                            <div className="forecast-temp">
+                                <p>{weatherData.Tue_temp}°C</p>
+                            </div>
+                        </div>
+                    </button>
+
+                    <button className="forecast-button"  onClick={() => {
+                        return( seti(21), search() );
+                    }}>
+                        <div className='cloud'>
+                            <h4>{weatherData.Wed_dt.day}</h4>
+                            <img src={wed_icon} height={60}></img>
+                            <div className="forecast-temp">
+                            <p>{weatherData.Wed_temp}°C</p>
                             </div>
                         </div>
                     </button>
                     
                     <button className="forecast-button" onClick={() => {
-                        return( seti(20), search() );
+                        return( seti(29), search() );
                     }}>
                         <div className='cloud'>
-                            <h4>Thu</h4>
-                            <img src={thu_icon} height={50}></img>
-                            <div className="forecastMax-Min_temp">
-                            <p> {weatherData.Thu_max}</p>
-                            <p> {weatherData.Thu_min}</p>
+                            <h4>{weatherData.Thu_dt.day}</h4>
+                            <img src={thu_icon} height={60}></img>
+                            <div className="forecast-temp">
+                                <p>{weatherData.Thu_temp}°C</p>
                             </div>
                         </div>
                     </button>
 
                     <button className="forecast-button"  onClick={() => {
-                        return( seti(25), search() );
+                        return( seti(37), search() );
                     }}>
                         <div className='cloud'>
-                            <h4>Fri</h4>
-                            <img src={fri_icon} height={50}></img>
-                            <div className="forecastMax-Min_temp">
-                            <p> {weatherData.Fri_max}</p>
-                            <p> {weatherData.Fri_min}</p>
+                            <h4>{weatherData.Fri_dt.day}</h4>
+                            <img src={fri_icon} height={60}></img>
+                            <div className="forecast-temp">
+                             <p>{weatherData.Fri_temp}°C</p>
                             </div>
                         </div>
-                    </button>
-
-                    <button className="forecast-button"   onClick={() => {
-                        return( seti(30), search() );
-                    }}>
-                        <div className='cloud'>
-                            <h4>Sat</h4>
-                            <img src={sat_icon} height={50}></img>
-                            <div className="forecastMax-Min_temp">
-                            <p> {weatherData.Sat_max}</p>
-                            <p> {weatherData.Sat_min}</p>
-                            </div>
-                        </div>
-                    </button>
-
-                    <button className="forecast-button"  onClick={() => {
-                        return( seti(35), search() );
-                    }}>
-                        <div className='cloud'>
-                            <h4>Sun</h4>
-                            <img src={sun_icon} height={50}></img>
-                            <div className="forecastMax-Min_temp">
-                            <p> {weatherData.Sun_max} </p>
-                            <p> {weatherData.Sun_min} </p>
-                            </div>
-                        </div>
-                    </button>  
+                    </button>    
                 </div>
             </div>
         </div>
